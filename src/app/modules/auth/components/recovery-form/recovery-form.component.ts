6de43e99063bb79e0abeee624695966b2d72ff0c
Daniel Hinostrosa <dhinostrosa@viamatica.com>
2026-03-26 01:24:50 -0500

    Proteger nuestras rutas con CanActivate

--- src/app/modules/auth/components/recovery-form/recovery-form.component.ts
+++ src/app/modules/auth/components/recovery-form/recovery-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '@services/auth.service';
 
 import { CustomValidators } from '@utils/validators';
 
@@ -24,12 +26,37 @@ export class RecoveryFormComponent {
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  token = '';
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    route.queryParamMap.subscribe((params) => {
+      const token = params.get('token');
+      if (token) {
+        this.token = token;
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 
   recovery() {
     if (this.form.valid) {
-      // Todo
+      this.status = 'loading';
+      const { newPassword } = this.form.getRawValue();
+      this.authService.changePassword(this.token, newPassword).subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/login']);
+        },
+        error: () => {
+          this.status = 'failed';
+        }
+      });
     } else {
       this.form.markAllAsTouched();
     }
